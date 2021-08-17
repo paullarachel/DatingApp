b5ca8a1ac5fdff7ff5461848f61e5faa06d58a67
Paulla Rachel <rachel.p@academico.ifrn.edu.br>
2021-08-17 14:59:12 -0300

    Fim da sessão 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,7 +12,7 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly DataContext _context; 
         public UsersController(DataContext context)
         {
             _context = context;
